5e073233fb19293ca07b2981d57c68942a260f20
Jinho Yoon <yoonjinho0318@gmail.com>
2023-02-09 14:43:30 -0500

    feat: add ast term type

--- lib/cst.ml
+++ lib/cst.ml
@@ -7,3 +7,13 @@ type obj =
   | Fun of string * obj (* fun x.M *)
   | App of obj * obj (* M N *)
   (* There's a couple more *)
+
+type term =
+  | Type of int (* Type i *)
+  | Nat (* Nat *)
+  | Var of string (* x *)
+  | Zero (* zero *)
+  | Succ of obj (* succ x *)
+  | Fun of int * obj (* fun x.M *)
+  | App of obj * obj (* M N *)
+  (* There's a couple more *)
