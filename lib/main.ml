541d76d04d3101e0b26cdb966bce7ff378f674c5
Jinho Yoon <yoonjinho0318@gmail.com>
2023-03-04 23:48:49 -0500

    refactor: remove helper function `loop` from the toplevel
    The function `loop` should be in the body of `parse` since `loop` should
    not be used outside of `parse`.
    
    + Rename variables to be more descriptive
    + Add a comment explaining the conversion from a lexing buffer to a
      token stream, in case I forget.

--- lib/main.ml
+++ lib/main.ml
@@ -1,11 +1,11 @@
 open Parser.MenhirLibParser.Inter
 
-let rec loop x = lazy (Buf_cons (Lexer.read x, loop x))
-
-let parse s =
-  let lexbuf = Lexing.from_string s in
-  let buffer = loop lexbuf in
-  match Parser.prog 50 buffer with
+let parse text =
+  (* Before parsing, we must generate a token stream from a lexer buffer,
+     which we then feed into the parser. *)
+  let rec loop lexbuf = lazy (Buf_cons (Lexer.read lexbuf, loop lexbuf)) in
+  let token_stream = loop (Lexing.from_string text) in
+  match Parser.prog 50 token_stream with
   | Parsed_pr (e, _) -> Some e
   | Fail_pr_full (_, _) -> None
   | _ -> None
