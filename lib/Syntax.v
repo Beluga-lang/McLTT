7944d6589a091db065feb8038f7a2232bc6da739
Jinho Yoon <yoonjinho0318@gmail.com>
2023-03-14 21:15:07 -0400

    feat: add ast type with explicit substitutions + context

--- lib/Syntax.v
+++ lib/Syntax.v
@@ -1,5 +1,7 @@
 Require Import String.
+Require Import List.
 
+(* CST term *)
 Module Cst.
 Inductive obj :=
   | TType : nat -> obj
@@ -11,3 +13,25 @@ Inductive obj :=
   | Pi : string -> obj -> obj -> obj
   | Var : string -> obj.
 End Cst.
+
+(* AST term *)
+Inductive exp : Set :=
+  (* Natural numbers *)
+  | Zero : exp
+  | Succ : exp
+  (* Type constructors *)
+  | Nat : exp
+  | Typ : nat -> exp
+  | Var : nat -> exp
+  (* Functions *)
+  | Fun : exp -> exp
+  | App : exp -> exp -> exp
+  (* Substitutions *)
+  | Sub : exp -> subst -> exp
+with subst : Set :=
+  | Id : subst
+  | Weaken : subst
+  | Compose : subst -> subst -> subst
+  | Extend : subst -> exp -> subst.
+
+Definition Ctx : Set := list exp.
