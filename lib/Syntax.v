7504c83e4028f76baf8ef0ffb76fc78d9ce41127
Jinho Yoon <yoonjinho0318@gmail.com>
2023-03-23 23:19:50 -0400

    refactor!: add namespacing prefixes to AST terms

--- lib/Syntax.v
+++ lib/Syntax.v
@@ -4,35 +4,35 @@ Require Import List.
 (* CST term *)
 Module Cst.
 Inductive obj : Set :=
-  | typ : nat -> obj
-  | nat : obj
-  | zero : obj
-  | succ : obj -> obj
-  | app : obj -> obj -> obj
-  | fn : string -> obj -> obj -> obj
-  | pi : string -> obj -> obj -> obj
-  | var : string -> obj.
+  | c_typ : nat -> obj
+  | c_nat : obj
+  | c_zero : obj
+  | c_succ : obj -> obj
+  | c_app : obj -> obj -> obj
+  | c_fn : string -> obj -> obj -> obj
+  | c_pi : string -> obj -> obj -> obj
+  | c_var : string -> obj.
 End Cst.
 
 (* AST term *)
 Inductive exp : Set :=
   (* Natural numbers *)
-  | zero : exp
-  | succ : exp -> exp
+  | a_zero : exp
+  | a_succ : exp -> exp
   (* Type constructors *)
-  | nat : exp
-  | typ : nat -> exp
-  | var : nat -> exp
+  | a_nat : exp
+  | a_typ : nat -> exp
+  | a_var : nat -> exp
   (* Functions *)
-  | fn : exp -> exp
-  | app : exp -> exp -> exp
+  | a_fn : exp -> exp -> exp
+  | a_app : exp -> exp -> exp
   (* Substitutions *)
-  | sub : exp -> subst -> exp
+  | a_sub : exp -> subst -> exp
 with subst : Set :=
-  | id : subst
-  | weaken : subst
-  | compose : subst -> subst -> subst
-  | extend : subst -> exp -> subst.
+  | a_id : subst
+  | a_weaken : subst
+  | a_compose : subst -> subst -> subst
+  | a_extend : subst -> exp -> subst.
 
 (* Some convenient infix notations *)
 Infix "∘" := compose (at level 70).
