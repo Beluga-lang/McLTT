cdd1661f5e8d0697b5c5ccae2d704fe8fd4de8c8
Junyoung/Clare Jang <jjc9310@gmail.com>
2024-01-21 15:15:17 -0500

    Add domain and related props

---
+++ theories/Core/Semantic/Domain.v
@@ -0,0 +1,55 @@
+From Mcltt Require Import Syntax.
+
+Reserved Notation "'env'".
+
+Inductive domain : Set :=
+| d_nat : domain
+| d_pi : domain -> env -> exp -> domain
+| d_univ : nat -> domain
+| d_zero : domain
+| d_succ : domain -> domain
+| d_fn : env -> exp -> domain
+| d_neut : domain -> domain_ne -> domain
+with domain_ne : Set :=
+(** Notice that the number x here is not a de Bruijn index but an absolute
+    representation of names.  That is, this number does not change relative to the
+    binding structure it currently exists in.
+ *)
+| d_var : forall (x : nat), domain_ne
+| d_app : domain_ne -> domain_nf -> domain_ne
+| d_natrec : env -> typ -> domain -> exp -> domain_ne -> domain_ne
+with domain_nf : Set :=
+| d_dom : domain -> domain -> domain_nf
+where "'env'" := (nat -> domain).
+
+#[global] Declare Custom Entry domain.
+#[global] Bind Scope exp_scope with domain.
+
+Notation "'d{{{' x '}}}'" := x (at level 0, x custom domain at level 99) : exp_scope.
+Notation "( x )" := x (in custom domain at level 0, x custom domain at level 60) : exp_scope.
+Notation "~ x" := x (in custom domain at level 0, x constr at level 0) : exp_scope.
+Notation "x" := x (in custom domain at level 0, x global) : exp_scope.
+Notation "'λ' p M" := (d_fn p M) (in custom domain at level 0, p custom domain at level 30, M custom exp at level 30) : exp_scope.
+Notation "f x .. y" := (d_app .. (d_app f x) .. y) (in custom domain at level 40, f custom domain, x custom domain at next level, y custom domain at next level) : exp_scope.
+Notation "'ℕ'" := d_nat (in custom domain) : exp_scope.
+Notation "'𝕌' @ n" := (d_univ n) (in custom domain at level 0, n constr at level 0) : exp_scope.
+Notation "'Π' a p B" := (d_pi a p B) (in custom domain at level 0, a custom domain at level 30, p custom domain at level 0, B custom exp at level 30) : exp_scope.
+Notation "'zero'" := d_zero (in custom domain at level 0) : exp_scope.
+Notation "'succ' m" := (d_succ m) (in custom domain at level 30, m custom domain at level 30) : exp_scope.
+Notation "'rec' m 'under' p 'return' P | 'zero' -> mz | 'succ' -> MS 'end'" := (d_natrec p P mz MS m) (in custom domain at level 0, P custom exp at level 60, mz custom domain at level 60, MS custom exp at level 60, p custom domain at level 60, m custom domain at level 60) : exp_scope.
+Notation "'!' n" := (d_var n) (in custom domain at level 0, n constr at level 0) : exp_scope.
+Notation "'⇑' a m" := (d_neut a m) (in custom domain at level 0, a custom domain at level 30, m custom domain at level 30) : exp_scope.
+Notation "'⇓' a m" := (d_dom a m) (in custom domain at level 0, a custom domain at level 30, m custom domain at level 30) : exp_scope.
+Notation "'⇑!' a n" := (d_neut a (d_var n)) (in custom domain at level 0, a custom domain at level 30, n constr at level 0) : exp_scope.
+
+Definition empty_env : env := fun x => d{{{ zero }}}.
+Definition extend_env (p : env) (d : domain) : env :=
+  fun n =>
+    match n with
+    | 0 => d
+    | S n' => p n'
+    end.
+Notation "p ↦ m" := (extend_env p m) (in custom domain at level 20, left associativity) : exp_scope.
+
+Definition drop_env (p : env) : env := fun n => p (S n).
+Notation "p '↯'" := (drop_env p) (in custom domain at level 10, p custom domain) : exp_scope.
---
+++ theories/Core/Semantic/Readback.v
@@ -0,0 +1,83 @@
+From Mcltt Require Import Base.
+From Mcltt Require Import Domain.
+From Mcltt Require Import Evaluate.
+From Mcltt Require Import Syntax.
+From Mcltt Require Import System.
+
+Reserved Notation "'Rnf' m 'in' s ↘ M" (in custom judg at level 80, m custom domain, s constr, M custom nf).
+Reserved Notation "'Rne' m 'in' s ↘ M" (in custom judg at level 80, m custom domain, s constr, M custom nf).
+Reserved Notation "'Rtyp' m 'in' s ↘ M" (in custom judg at level 80, m custom domain, s constr, M custom nf).
+
+Generalizable All Variables.
+
+Inductive read_nf : nat -> domain_nf -> nf -> Prop :=
+| read_nf_type :
+  `( {{ Rtyp a in s ↘ A }} ->
+     {{ Rnf ⇓ 𝕌@i a in s ↘ A }} )
+| read_nf_zero :
+  `( {{ Rnf ⇓ ℕ zero in s ↘ zero }} )
+| read_nf_succ :
+  `( {{ Rnf ⇓ ℕ m in s ↘ M }} ->
+     {{ Rnf ⇓ ℕ (succ m) in s ↘ succ M }} )
+| read_nf_nat_neut :
+  `( {{ Rne m in s ↘ M }} ->
+     {{ Rnf ⇓ ℕ (⇑ ℕ m) in s ↘ ⇑ M }} )
+| read_nf_fn :
+  (* Nf of arg type *)
+  `( {{ Rtyp a in s ↘ A }} ->
+
+     (* Nf of eta-expanded body *)
+     {{ $| m & ⇑! a s |↘ m' }} ->
+     {{ ⟦ B ⟧ p ↦ ⇑! a s ↘ b }} ->
+     {{ Rnf ⇓ b m' in S s ↘ M }} ->
+
+     {{ Rnf ⇓ (Π a p B) m in s ↘ λ A M }} )
+| read_nf_neut :
+  `( {{ Rne m in s ↘ M }} ->
+     {{ Rnf ⇓ (⇑ a b) (⇑ c m) in s ↘ ⇑ M }} )
+where "'Rnf' m 'in' s ↘ M" := (read_nf s m M) (in custom judg) : exp_scope
+with read_ne : nat -> domain_ne -> ne -> Prop :=
+| read_ne_var :
+  `( {{ Rne !x in s ↘ #(s - x - 1) }} )
+| read_ne_app :
+  `( {{ Rne m in s ↘ M }} ->
+     {{ Rnf n in s ↘ N }} ->
+     {{ Rne m n in s ↘ M N }} )
+| read_ne_natrec :
+  (* Nf of motive *)
+  `( {{ ⟦ B ⟧ p ↦ ⇑! ℕ s ↘ b }} ->
+     {{ Rtyp b in S s ↘ B' }} ->
+
+     (* Nf of mz *)
+     {{ ⟦ B ⟧ p ↦ zero ↘ bz }} ->
+     {{ Rnf ⇓ bz mz in s ↘ MZ }} ->
+
+     (* Nf of MS *)
+     {{ ⟦ B ⟧ p ↦ succ (⇑! ℕ s) ↘ bs }} ->
+     {{ ⟦ MS ⟧ p ↦ ⇑! ℕ s ↦ ⇑! b (S s) ↘ ms }} ->
+     {{ Rnf ⇓ bs ms in S (S s) ↘ MS' }} ->
+
+     (* Ne of m *)
+     {{ Rne m in s ↘ M }} ->
+
+     {{ Rne rec m under p return B | zero -> mz | succ -> MS end in s ↘ rec M return B' | zero -> MZ | succ -> MS' end }} )
+where "'Rne' m 'in' s ↘ M" := (read_ne s m M) (in custom judg) : exp_scope
+with read_typ : nat -> domain -> nf -> Prop :=
+| read_typ_univ :
+  `( {{ Rtyp 𝕌@i in s ↘ Type@i }} )
+| read_typ_nat :
+  `( {{ Rtyp ℕ in s ↘ ℕ }} )
+| read_typ_pi :
+  (* Nf of arg type *)
+  `( {{ Rtyp a in s ↘ A }} ->
+
+     (* Nf of ret type *)
+     {{ ⟦ B ⟧ p ↦ ⇑! a s ↘ b }} ->
+     {{ Rtyp b in S s ↘ B' }} ->
+
+     {{ Rtyp Π a p B in s ↘ Π A B' }})
+| read_typ_neut :
+  `( {{ Rne b in s ↘ B }} ->
+     {{ Rtyp ⇑ a b in s ↘ ⇑ B }})
+where "'Rtyp' m 'in' s ↘ M" := (read_typ s m M) (in custom judg) : exp_scope
+.
