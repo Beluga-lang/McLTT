76a7a58e82ec266a819560c91a84605858d39b54
HuStmpHrrr <fdhzs2010@hotmail.com>
2024-05-17 18:38:25 -0400

    fix load path

--- theories/Core/Semantic/PER/CoreTactics.v
+++ theories/Core/Semantic/PER/CoreTactics.v
@@ -1,6 +1,7 @@
 From Coq Require Import Lia PeanoNat Relation_Definitions RelationClasses.
 From Equations Require Import Equations.
-From Mcltt Require Import Base Evaluation LibTactics PER.Definitions Readback.
+From Mcltt Require Import Base LibTactics.
+From Mcltt.Core Require Import Evaluation PER.Definitions Readback.
 Import Domain_Notations.
 
 Ltac destruct_rel_by_assumption in_rel H :=
