af0665332553895bd7c640fd50a5d4bd3d4ccf1f
Junyoung/"Clare" Jang <jjc9310@gmail.com>
2024-05-28 14:37:29 -0400

    Fix some proofs

--- theories/Core/Semantic/Domain/Lemmas.v
+++ theories/Core/Semantic/Domain/Lemmas.v
@@ -4,11 +4,11 @@ Import Domain_Notations.
 Lemma drop_env_empty_env_no_op :
   d{{{ empty_env ↯ }}} = empty_env.
 Proof.
-  unfold drop_env; simpl; reflexivity.
+  reflexivity.
 Qed.
 
 Lemma drop_env_extend_env_cancel : forall p a,
     d{{{ (p ↦ a) ↯ }}} = p.
 Proof.
-  unfold drop_env; simpl; reflexivity.
+  reflexivity.
 Qed.
