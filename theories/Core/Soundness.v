f13036b13f6d8e05eb88b4a27dabe1e99b8d5375
Junyoung/Clare Jang <jjc9310@gmail.com>
2024-06-13 04:12:26 -0400

    Extend export list of Soundness

--- theories/Core/Soundness.v
+++ theories/Core/Soundness.v
@@ -1,5 +1,6 @@
 From Mcltt Require Import Base LibTactics.
-From Mcltt.Core Require Import Semantic.Domain Semantic.NbE System.
+From Mcltt.Core Require Import Semantic.Domain.
+From Mcltt.Core Require Export Semantic.NbE Soundness.LogicalRelation System.
 Import Domain_Notations.
 
 Theorem soundness : forall {Γ M A},
