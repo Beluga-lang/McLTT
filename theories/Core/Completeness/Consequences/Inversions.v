71820ec38d02a3ded72c5c0946ccd4488955187a
Junyoung/Clare Jang <jjc9310@gmail.com>
2024-06-14 16:12:37 -0400

    Fix inversion lemmas

--- theories/Core/Completeness/Consequences/Inversions.v
+++ theories/Core/Completeness/Consequences/Inversions.v
@@ -4,52 +4,32 @@ From Mcltt.Core Require Import Completeness Semantic.Realizability Completeness.
 From Mcltt.Core Require Export SystemOpt CoreInversions.
 Import Domain_Notations.
 
-Corollary wf_zero_inversion : forall Γ A,
+Corollary wf_zero_inversion' : forall Γ A,
     {{ Γ ⊢ zero : A }} ->
     exists i, {{ Γ ⊢ ℕ ≈ A : Type@i }}.
 Proof with mautosolve 4.
-  intros * H.
-  dependent induction H; try mautosolve.
-  - (* wf_conv *)
-    assert (exists i : nat, {{ Γ ⊢ ℕ ≈ A : Type@i }}) as [j] by eauto...
-  - (* wf_cumu *)
-    assert (exists j : nat, {{ Γ ⊢ ℕ ≈ Type@i : Type@j }}) as [j contra] by eauto.
-    contradict contra...
+  intros * []%wf_zero_inversion%typ_subsumption_left_nat...
 Qed.
 
-Corollary wf_succ_inversion : forall Γ A M,
+#[export]
+Hint Resolve wf_zero_inversion' : mcltt.
+
+Corollary wf_succ_inversion' : forall Γ A M,
     {{ Γ ⊢ succ M : A }} ->
     {{ Γ ⊢ M : ℕ }} /\ exists i, {{ Γ ⊢ ℕ ≈ A : Type@i }}.
 Proof with mautosolve.
-  intros * H.
-  dependent induction H; try (split; mautosolve).
-  - (* wf_conv *)
-    assert ({{ Γ ⊢ M : ℕ }} /\ exists i : nat, {{ Γ ⊢ ℕ ≈ A : Type@i }}) as [] by eauto.
-    destruct_conjs...
-  - (* wf_cumu *)
-    assert ({{ Γ ⊢ M : ℕ }} /\ exists j : nat, {{ Γ ⊢ ℕ ≈ Type@i : Type@j }}) as [? [? contra]] by eauto.
-    contradict contra...
+  intros * [? []%typ_subsumption_left_nat]%wf_succ_inversion...
 Qed.
 
-Corollary wf_fn_inversion : forall {Γ A M C},
+#[export]
+Hint Resolve wf_succ_inversion' : mcltt.
+
+Corollary wf_fn_inversion' : forall {Γ A M C},
     {{ Γ ⊢ λ A M : C }} ->
-    exists i B, {{ Γ ⊢ A : Type@i }} /\ {{ Γ, A ⊢ B : Type@i }} /\ {{ Γ ⊢ Π A B ≈ C : Type@i }}.
-Proof with mautosolve 4.
-  intros * H.
-  dependent induction H.
-  - (* wf_fn *)
-    gen_presups.
-    exvar nat ltac:(fun i => assert {{ Γ ⊢ A : Type @ i }} by (eapply lift_exp_max_left; eauto)).
-    exvar nat ltac:(fun i => assert {{ Γ, A ⊢ B : Type @ i }} by (eapply lift_exp_max_right; eauto)).
-    do 2 eexists.
-    repeat split...
-  - (* wf_conv *)
-    specialize (IHwf_exp _ _ eq_refl) as [i0 [? [? []]]].
-    exists (max i i0).
-    eexists.
-    repeat split;
-      mauto 4 using lift_exp_max_right, lift_exp_eq_max_left, lift_exp_eq_max_right.
-  - (* wf_cumu *)
-    specialize (IHwf_exp _ _ eq_refl) as [? [? [? [? contra]]]].
-    contradict contra...
+    exists B, {{ Γ, A ⊢ M : B }} /\ exists i, {{ Γ ⊢ Π A B ≈ C : Type@i }}.
+Proof with mautosolve.
+  intros * [? [? []%typ_subsumption_left_pi]]%wf_fn_inversion...
 Qed.
+
+#[export]
+Hint Resolve wf_fn_inversion' : mcltt.
--- theories/Core/Syntactic/CoreTypeInversions.v
+++ theories/Core/Syntactic/CoreTypeInversions.v
@@ -6,9 +6,9 @@ Import Syntax_Notations.
 Lemma wf_univ_inversion : forall {Γ i A},
     {{ Γ ⊢ Type@i : A }} ->
     {{ Γ ⊢ Type@(S i) ⊆ A }}.
-Proof.
+Proof with mautosolve.
   intros * H.
-  dependent induction H; mautosolve.
+  dependent induction H...
 Qed.
 
 #[export]
@@ -17,10 +17,10 @@ Hint Resolve wf_univ_inversion : mcltt.
 Lemma wf_nat_inversion : forall Γ A,
     {{ Γ ⊢ ℕ : A }} ->
     {{ Γ ⊢ Type@0 ⊆ A }}.
-Proof.
+Proof with mautosolve 4.
   intros * H.
   assert (forall i, 0 <= i) by lia.
-  dependent induction H; mautosolve 4.
+  dependent induction H...
 Qed.
 
 #[export]
@@ -29,10 +29,13 @@ Hint Resolve wf_nat_inversion : mcltt.
 Lemma wf_pi_inversion : forall {Γ A B C},
     {{ Γ ⊢ Π A B : C }} ->
     exists i, {{ Γ ⊢ A : Type@i }} /\ {{ Γ, A ⊢ B : Type@i }} /\ {{ Γ ⊢ Type@i ⊆ C }}.
-Proof.
+Proof with mautosolve 4.
   intros * H.
-  dependent induction H; assert {{ ⊢ Γ }} by mauto 3; try solve [eexists; mauto 4];
-    specialize (IHwf_exp _ _ eq_refl); destruct_conjs; eexists; repeat split; mauto 3.
+  dependent induction H;
+    try specialize (IHwf_exp _ _ eq_refl);
+    destruct_conjs;
+    assert {{ ⊢ Γ }} by mauto 3;
+    eexists; split...
 Qed.
 
 #[export]
@@ -47,7 +50,7 @@ Proof with mautosolve 4.
   assert {{ Γ, A ⊢ Type@j ⊆ Type@j[Wk] }} by mauto 4.
   assert {{ Γ, A ⊢ Type@j[Wk] ⊆ Type@i[Wk] }} by mauto 4.
   assert {{ Γ, A ⊢ Type@i[Wk] ⊆ Type@i }} by mauto 4.
-  assert {{ Γ, A ⊢ Type@j ⊆ Type@i }}...
+  enough {{ Γ, A ⊢ Type@j ⊆ Type@i }}...
 Qed.
 
 #[export]
