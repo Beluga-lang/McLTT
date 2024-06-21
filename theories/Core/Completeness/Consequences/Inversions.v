6c7714bcdfee74d3d016081980d92222ba5007fb
HuStmpHrrr <fdhzs2010@hotmail.com>
2024-06-21 19:59:56 -0400

    comment out all stuff

--- theories/Core/Completeness/Consequences/Inversions.v
+++ theories/Core/Completeness/Consequences/Inversions.v
@@ -4,32 +4,32 @@ From Mcltt.Core Require Import Completeness Semantic.Realizability Completeness.
 From Mcltt.Core Require Export SystemOpt CoreInversions.
 Import Domain_Notations.
 
-Corollary wf_zero_inversion' : forall Γ A,
-    {{ Γ ⊢ zero : A }} ->
-    exists i, {{ Γ ⊢ ℕ ≈ A : Type@i }}.
-Proof with mautosolve 4.
-  intros * []%wf_zero_inversion%typ_subsumption_left_nat...
-Qed.
+(* Corollary wf_zero_inversion' : forall Γ A, *)
+(*     {{ Γ ⊢ zero : A }} -> *)
+(*     exists i, {{ Γ ⊢ ℕ ≈ A : Type@i }}. *)
+(* Proof with mautosolve 4. *)
+(*   intros * []%wf_zero_inversion%typ_subsumption_left_nat... *)
+(* Qed. *)
 
-#[export]
-Hint Resolve wf_zero_inversion' : mcltt.
+(* #[export] *)
+(* Hint Resolve wf_zero_inversion' : mcltt. *)
 
-Corollary wf_succ_inversion' : forall Γ A M,
-    {{ Γ ⊢ succ M : A }} ->
-    {{ Γ ⊢ M : ℕ }} /\ exists i, {{ Γ ⊢ ℕ ≈ A : Type@i }}.
-Proof with mautosolve.
-  intros * [? []%typ_subsumption_left_nat]%wf_succ_inversion...
-Qed.
+(* Corollary wf_succ_inversion' : forall Γ A M, *)
+(*     {{ Γ ⊢ succ M : A }} -> *)
+(*     {{ Γ ⊢ M : ℕ }} /\ exists i, {{ Γ ⊢ ℕ ≈ A : Type@i }}. *)
+(* Proof with mautosolve. *)
+(*   intros * [? []%typ_subsumption_left_nat]%wf_succ_inversion... *)
+(* Qed. *)
 
-#[export]
-Hint Resolve wf_succ_inversion' : mcltt.
+(* #[export] *)
+(* Hint Resolve wf_succ_inversion' : mcltt. *)
 
-Corollary wf_fn_inversion' : forall {Γ A M C},
-    {{ Γ ⊢ λ A M : C }} ->
-    exists B, {{ Γ, A ⊢ M : B }} /\ exists i, {{ Γ ⊢ Π A B ≈ C : Type@i }}.
-Proof with mautosolve.
-  intros * [? [? []%typ_subsumption_left_pi]]%wf_fn_inversion...
-Qed.
+(* Corollary wf_fn_inversion' : forall {Γ A M C}, *)
+(*     {{ Γ ⊢ λ A M : C }} -> *)
+(*     exists B, {{ Γ, A ⊢ M : B }} /\ exists i, {{ Γ ⊢ Π A B ≈ C : Type@i }}. *)
+(* Proof with mautosolve. *)
+(*   intros * [? [? []%typ_subsumption_left_pi]]%wf_fn_inversion... *)
+(* Qed. *)
 
-#[export]
-Hint Resolve wf_fn_inversion' : mcltt.
+(* #[export] *)
+(* Hint Resolve wf_fn_inversion' : mcltt. *)
