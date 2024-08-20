358c3db1771ad6ad092fc211febf88181db6f7c8
HuStmpHrrr <fdhzs2010@hotmail.com>
2024-08-20 11:42:27 -0400

    what's wrong with Equations

--- theories/Extraction/Subtyping.v
+++ theories/Extraction/Subtyping.v
@@ -40,8 +40,31 @@ Equations subtyping_nf_impl A B : { {{ ⊢anf A ⊆ B }} } + {~ {{ ⊢anf A ⊆ 
   }.
 
 
+Inductive subtyping_order G A B :=
+| subtyping_order_run :
+  nbe_ty_order G A ->
+  nbe_ty_order G B ->
+  subtyping_order G A B.
+#[local]
+  Hint Constructors subtyping_order : mcltt.
 
-(* Equations subtyping_impl Γ A B : bool := *)
-(* | Γ, A, B => { *)
-(*     let (w, _) := nbe_impl Γ  *)
-(*   } *)
+#[local]
+  Ltac subtyping_impl_tac1 :=
+  match goal with
+  | H : subtyping_order _ _ _ |- _ => progressive_invert H
+  end.
+
+#[local]
+  Ltac subtyping_impl_tac :=
+  repeat subtyping_impl_tac1; try econstructor; mauto.
+
+Equations? subtyping_impl G A B (H1 : nbe_ty_order G A) (H2 : nbe_ty_order G B) :
+  { {{G ⊢a A ⊆ B}} } + { ~ {{ G ⊢a A ⊆ B }} } :=
+| G, A, B with nbe_ty_impl G A H1 => {
+  | @exist a _ with nbe_ty_impl G B H2 => {
+    | @exist b _ with subtyping_nf_impl a b => {
+      | left _ => _;
+      | right _ => _
+      }
+    }
+  }.
