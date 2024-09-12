d25dc0a9671860410ad82677f39b81049961a563
Junyoung/"Clare" Jang <jjc9310@gmail.com>
2024-09-12 17:05:10 -0400

    Update Ocaml-related thingy (#195)

--- driver/Main.ml
+++ driver/Main.ml
@@ -4,26 +4,12 @@ open Parser
 open MenhirLibParser.Inter
 open Entrypoint
 
-let main ?(default_fp = "") () =
-  let fp =
-    if default_fp <> ""
-    then default_fp
-    else
-      begin
-        print_string "File path to load: ";
-        read_line ()
-      end
-  in
-  let chan = open_in fp in
-  (* Before parsing, we must generate a token stream from a lexer buffer,
+let main filename =
+  open_in filename
+  |> Lexing.from_channel
+  (* Before parsing, we must generate a stream of tokens from a lexer buffer,
      which we then feed into the parser. *)
-  let rec loop lexbuf =
-    lazy (
-        try
-          Buf_cons (Lexer.read lexbuf, loop lexbuf)
-        with
-        | Failure s -> prerr_string s; raise Exit) in
-  let token_stream = loop (Lexing.from_channel chan) in
-  let res = Entrypoint.main 500 token_stream in
-  Format.printf "%a@."
-    PrettyPrinter.format_main_result res
+  |> Lexer.lexbuf_to_token_buffer
+  |> Entrypoint.main 500
+  |> Format.printf "%a@."
+       PrettyPrinter.format_main_result
