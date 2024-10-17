2085036c5d9dc3e721e22068a664629abf909cee
Junyoung/Clare Jang <jjc9310@gmail.com>
2024-10-17 03:50:31 -0400

    Restore Semantics

--- theories/Core/Semantic/Evaluation/Tactics.v
+++ theories/Core/Semantic/Evaluation/Tactics.v
@@ -7,8 +7,9 @@ Ltac simplify_evals :=
   functional_eval_rewrite_clear;
   clear_dups;
   repeat (match_by_head eval_exp ltac:(fun H => directed dependent destruction H)
-          || match_by_head eval_app ltac:(fun H => directed dependent destruction H)
           || match_by_head eval_natrec ltac:(fun H => directed dependent destruction H)
+          || match_by_head eval_app ltac:(fun H => directed dependent destruction H)
+          || match_by_head eval_eqrec ltac:(fun H => directed dependent destruction H)
           || match_by_head eval_sub ltac:(fun H => directed dependent destruction H));
   functional_eval_rewrite_clear;
   clear_dups.
