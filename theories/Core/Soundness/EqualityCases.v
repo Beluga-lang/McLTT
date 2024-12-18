ce7f979fb96d4bb8e86df2c0241be14daf588dcf
HuStmpHrrr <fdhzs2010@hotmail.com>
2024-12-17 20:55:04 -0800

    Revert "revert"
    This reverts commit a55fc6760ced90032bc1a93267a06c897dfa5504.

--- theories/Core/Soundness/EqualityCases.v
+++ theories/Core/Soundness/EqualityCases.v
@@ -101,68 +101,6 @@ Lemma glu_rel_eq_eqrec : forall Γ A i M1 M2 B j BR N,
     {{ Γ, A ⊩ BR : B[Id,,#0,,refl A[Wk] #0] }} ->
     {{ Γ ⊩ N : Eq A M1 M2 }} ->
     {{ Γ ⊩ eqrec N as Eq A M1 M2 return B | refl -> BR end : B[Id,,M1,,M2,,N] }}.
-Proof.
-  intros * HA HM1 HM2 HB HBR HN.
-  assert {{ ⊩ Γ }} by mauto.
-  assert {{ ⊩ Γ }} as [SbΓ] by mauto.
-  saturate_syn_judge.
-
-  assert {{ Γ ⊩s Id,,M1 : Γ, A }} by (eapply glu_rel_sub_extend; mauto 3; bulky_rewrite).
-  assert {{ ⊩ Γ , A }} by mauto.
-  assert {{ ⊩ Γ , A }} as [SbΓA] by mauto.
-  assert {{ Γ ⊩s Id,,M1,,M2 : Γ, A, A[Wk] }} by (eapply glu_rel_sub_extend; mauto 4).
-  assert {{ ⊩ Γ , A, A[Wk] }} by mauto.
-  assert {{ ⊩ Γ , A, A[Wk] }} as [SbΓAA] by mauto.
-  assert {{ Γ ⊩s Id,,M1,,M2,,N : Γ, A, A[Wk], Eq A[Wk ∘ Wk] #1 #0 }}.
-  {
-    eapply glu_rel_sub_extend; mauto 4.
-    eapply glu_rel_eq.
-    - mauto.
-    - rewrite <- @exp_eq_sub_compose_typ with (i := i); mauto 3.
-    - rewrite <- @exp_eq_sub_compose_typ with (i := i); mauto 3.
-  }
-  saturate_syn_judge.
-
-  invert_sem_judge1.
-  invert_sem_judge1.
-  invert_sem_judge1.
-  invert_sem_judge1.
-  invert_sem_judge1.
-  invert_sem_judge1.
-  invert_sem_judge1.
-  invert_sem_judge1.
-
-
-Ltac invert_glu_rel_sub H :=
-  (unshelve eapply (glu_rel_sub_clean_inversion3 _ _) in H; shelve_unifiable; [eassumption | eassumption |])
-  + (unshelve eapply (glu_rel_sub_clean_inversion2 _) in H; shelve_unifiable; [eassumption |];
-     destruct H as [? []])
-  + (unshelve eapply (glu_rel_sub_clean_inversion1 _) in H; shelve_unifiable; [eassumption |];
-     destruct H as [? []])
-  + (inversion H; subst).
-
-unshelve eapply (glu_rel_sub_clean_inversion3 _ _) in H10; shelve_unifiable.
-eassumption.
-eassumption.
-invert_glu_rel_sub H10.
-
-
-  invert_sem_judge1.
-
-  eexists; split; eauto.
-  exists j; intros.
-  assert {{ Δ ⊢s σ : Γ }} by mauto 4.
-  apply_glu_rel_judge.
-  repeat invert_glu_rel1.
-  saturate_glu_typ_from_el.
-  unify_glu_univ_lvl i.
-  deepexec (glu_univ_elem_per_univ i) ltac:(fun H => pose proof H).
-  match_by_head per_univ ltac:(fun H => destruct H).
-  do 2 deepexec (glu_univ_elem_per_elem i) ltac:(fun H => pose proof H; fail_at_if_dup ltac:(4)).
-  saturate_glu_info.
-
-  econstructor.
-
 Admitted.
 
 #[export]
